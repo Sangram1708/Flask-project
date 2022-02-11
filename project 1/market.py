26383553437ed2fe9e323f5f637778acaf733880
Sangram <sangramnayak.1708@gmail.com>
2022-02-11 21:38:45 +0530

    commited till templates

--- project 1/market.py
+++ project 1/market.py
@@ -1,10 +1,16 @@
-from flask import Flask,render_template
+from turtle import title
+from flask import Flask,render_template,url_for
 
 app=Flask(__name__)
 @app.route("/")
 @app.route("/home")
-def home():
-    return render_template("home.html")
+def home_page():
+    return render_template("home.html",title="home")
+
+
+@app.route("/market")
+def market_page():
+    return render_template("market.html",title="market")
 
     
 if __name__=="__main__":
